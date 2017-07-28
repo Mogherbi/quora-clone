20ced88fe974644021857a165fb11b38d8be7d57
bestofthesoul <bestofthesoul@gmail.com>
2017-07-28 13:10:29 +0800

    i had just inserted gem bcrypt, had a users table with email and password, for my user authentication purpose

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+
+  has_secure_password #this is a method inside gem bcrypt that allows user to encrypt the password given, and able to authenticate it automatically
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+end
